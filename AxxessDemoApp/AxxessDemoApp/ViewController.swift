0dc4f316586675664f1a553a9ef710a4cbc958d0
Rahul <rahul.dange1989@gmail.com>
2020-08-12 16:32:59 +0530

    - Created AxessDemoApp project. - Added Podfile with required third party libraries.

---
+++ AxxessDemoApp/AxxessDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AxxessDemoApp
+//
+//  Created by Rahul Dange on 12/08/20.
+//  Copyright © 2020 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
